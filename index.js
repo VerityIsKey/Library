ff2a5da3c314e4fd484a27d9bca3f5da4ffb22ff
Jared <jaredhessworks@gmail.com>
2023-07-15 00:52:14 -0400

    add object constructor for the book objects

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 //Array that contains the current library of book objects
 let myLibrary = [];
 
-function book() {
-
+function book(title, author, numberOfPages, isRead) {
+    this.title = title
+    this.author = author
+    this.numberOfPages = numberOfPages
+    this.isRead = isRead
 }
 
 function addBookToLibrary() {
